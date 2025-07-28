bc218e81293e21a7b09fa8a53602a7a4bb51cf35
vinaykumar2705-coder <vinaykumargokinepalli@gmail.com>
2025-07-28 22:54:56 +0530

    Add files via upload

---
+++ 10july/index.py
@@ -0,0 +1,75 @@
+#varibales and their sum
+
+a=10
+b=27
+print("sum=",a+b)
+
+#subtract two numbers and print their result
+
+a=5
+b=7
+print("sub=",a-b)
+
+#mulitiply and store in third variable
+a=10
+b=9
+c=a*b
+print(c)
+
+#print the result with and without decimals
+a=10
+b=3
+print("with decimals",a/b)
+print("without decimals",a//b)
+
+#floor division to divide 17 by 4
+a=17
+b=5
+print(a//b)
+
+#module operator
+
+a=25
+b=6
+print(a%b)
+
+#print the square of a number
+
+a=9
+print(a*a)
+
+#three variables and their avg
+x=6
+y=7
+z=9
+print("avg=",(x+y+z)/3)
+
+#cube of a number
+
+a=3
+print(a*3)
+
+#area of rectangle
+
+length=6
+width=4
+print("area of rectangle=",length*width)
+
+#percentage of marks
+total_marks=450
+obtained_marks=375
+print((total_marks/obtained_marks)*100)
+
+#calculates formula
+a=4
+b=6
+c=3
+print((a+b)*c)
+
+#reassigning
+
+a=10
+print(a)
+a=12
+print("after reassigning",a)
+
